7a177793dd35432a4ff5a4c6d323e630dde9f582
wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-12-06 17:54:06 +0530

    fix: db client.

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,13 +2,13 @@ import pg, { Pool } from "pg";
 
 const { Client } = pg;
 
-const pgInstance = new Client({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 54320,
-  database: "trainn",
-});
+// const pgInstance = new Client({
+//   user: "postgres",
+//   password: "postgres",
+//   host: "localhost",
+//   port: 54320,
+//   database: "trainn",
+// });
 
 const pgPoolInstance = new Pool({
   // user: "postgres",
@@ -23,4 +23,5 @@ const pgPoolInstance = new Pool({
   database: process.env.DB_NAME,
 });
 
-export { pgPoolInstance, pgInstance };
+// export { pgPoolInstance, pgInstance };
+export { pgPoolInstance };
